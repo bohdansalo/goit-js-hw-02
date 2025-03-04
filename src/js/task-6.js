6703afc36eaeb2239210dca2b543197d02e534d5
Богдан <bohdansalo31@gmail.com>
2025-03-04 17:25:26 +0200

    fix

---
+++ src/js/task-6.js
@@ -0,0 +1,23 @@
+let input;
+const numbers = [];
+let total = 0;
+
+while (true) {
+  const input = prompt("Введіть число");
+
+  if (!input) {
+    break;
+  }
+
+  if (isNaN(Number(input))) {
+    alert("Було введено не число, попробуйте ще раз");
+    continue;
+  }
+  numbers.push(Number(input));
+}
+
+for (let number of numbers) {
+  total += number;
+}
+
+console.log(`Загальна сума чисел дорівнює ${total}`);
